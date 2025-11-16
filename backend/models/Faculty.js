3237d12d79dde0eafaff6bb81d0653bb5ab90cb7
Punya Udayasiri <dka.punya@gmail.com>
2025-11-16 10:10:18 +0530

    Delete backend directory

--- backend/models/Faculty.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const FacultySchema = new mongoose.Schema({
-  name: { type: String, required: true, unique: true },
-  departments: [{ type: String }]
-});
-
-module.exports = mongoose.model("Faculty", FacultySchema);
